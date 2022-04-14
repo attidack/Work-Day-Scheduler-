7bbe9b0f68b21aa1be979a99c8b554668c21df66
attidack <attidack@gmail.com>
2022-04-14 14:32:48 -0600

    inital commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var time = [9,10,11,12,1,2,3,4,5]
+var container = $('.container')
+// time array,
+for (const timeStep of time) {
+    var dateRow = $("<div>")
+    .addClass("row");
+    var timeCol =$("<div>")
+    
+  }
+  
