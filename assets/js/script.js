36e41f2664b9b2c57cc7081904eee61acf60c221
Jon Bird <attid@Jons-Laptop>
2022-04-18 02:09:06 -0600

    got time block structure working

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,17 @@ var container = $('.container')
 // time array,
 for (const timeStep of time) {
     var dateRow = $("<div>")
-    .addClass("row");
+    .addClass("row time-block");
     var timeCol =$("<div>")
+    .text('time')
     .addClass("col-1 hour");
     var textCol = $('<div>')
-    .addClass("col-10 textarea");
+    .addClass("col-10 description")
+    .attr("id", "textarea")
+    .text('message');
     var saveBtns = $('<div>')
     .addClass("col-1 saveBtn")
+    .text('save');
     container.append(dateRow);
     dateRow.append(timeCol, textCol, saveBtns);
     console.log("1")
