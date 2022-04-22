08015ab16a6de2f888901c7e57cc6f0f7abe0d83
attidack <attidack@gmail.com>
2022-04-21 21:42:58 -0600

    final js commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,72 @@
-var time = [9,10,11,12,1,2,3,4,5]
+var time = ['9am','10am',"11am","12pm","1pm","2pm","3pm","4pm","5pm"]
 var container = $('.container')
-var timeAmPm = $('.hour').text("9")
+var timeAmPm = $('.hour').text()
 var scheduleDataObj = {}
+var scheduleItem = {}
 $("#currentDay").text(moment().format('MMMM Do YYYY'));
+var currentTime = moment().format('ha')
+
 
 function timeRowIteration(){
+  
   // time array,
-  for (const timeStep of time) {
+  for (let i = 0; i < time.length; i++) {
     var dateRow = $("<div>")
     .addClass("row time-block")
-    .attr("data-row-id", timeStep);
     var timeCol =$("<div>")
-    .text(timeStep)
+    .text(time[i])
     .addClass("col-1 hour");
-    if (timeStep < 12 && timeStep > 8) {
-      timeCol.text(timeStep+"am")
-    }else{
-      timeCol.text(timeStep+"pm")
-    };
     var textCol = $('<textarea>')
     .addClass("col-10 description")
-    .text('message');
+    .text('');
     var saveBtns = $('<div>')
-    .addClass("col-1 saveBtn")
-    .text('save');
+    .addClass("col-1 saveBtn fa-solid fa-save d-flex justify-content-center align-items-center");
     saveBtns.on("click", handleSave )
     container.append(dateRow);
     dateRow.append(timeCol, textCol, saveBtns);
+    var difference = moment(time[i], "ha") - moment(currentTime, 'ha')
+    if (difference < 0) {
+      textCol.addClass("past")
+    }else if(difference === 0) {
+      textCol.addClass('present')
+    }else{
+      textCol.addClass('future')
+    }console.log(difference)
   }
 };
+timeRowIteration()
 
 function handleSave(e){
   var textBoxContents = e.target.previousElementSibling.value
   var timePosition = $(this).siblings(".hour").text()
   var localid = $(this).parent("div").attr("data-row-id")
   var scheduleDataObj =  {
-    id: localid,
     time: timePosition,
     message: textBoxContents
   }
-  localStorage.setItem(localid, JSON.stringify(scheduleDataObj));
+  scheduleItem[scheduleDataObj.time] = scheduleDataObj.message
+  localStorage.setItem("schedule", JSON.stringify(scheduleItem));
 }
-
-
-
-function loadSchedule(scheduleDataObj){
-  const schedule = JSON.parse(localStorage.getItem('9'))
-  if (schedule == null || schedule == "") {
-    timeRowIteration()
-  }else{
-    $('.hour')
-    // for (let index = 0; index < schedule.length; index++) {
-    //   const element = array[index];
-      
-    // }
-    timeRowIteration()
-
-  }
-  // if (schedule.id = ) {
-    
-  // }
-  
-
-  console.log(timeAmPm)
+function setItems(){
+  $.each(scheduleItem, function(key, value){
+    var hour = key;
+    var message = value;
+    var time = $('.time-block').find(".hour").filter(function(){
+      return $(this).text() === hour
+    })
+    var messageUpdate = time.siblings('.description')
+    messageUpdate.text(message)
+  })
+}
+function loadSchedule(){
+  scheduleItem = JSON.parse(localStorage.getItem('schedule'))
+  if (!scheduleItem) {
+    scheduleItem = {}
+  }  
+  $.each(scheduleItem, function(key, value){
+    var hour= key;
+    var message = value;
+ })
+ setItems()
 }
 loadSchedule();
